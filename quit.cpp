1e4470fd8c0ff559c1e05ddc689c12035aad83ef
manishtyagi5 <manishtyagi.contact@gmail.com>
2023-12-30 09:26:19 +0530

    abcd

---
+++ quit.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int helping()
+{
+    return 100;
+}
+int main()
+{
+    cout<<"Hello World"<<endl;
+    cout<<"from earth"<<endl;
+    cout<<"from vishnu"<<endl;
+    int a = 10+5;
+    cout<<a<<endl;
+    int x = helping();
+}
