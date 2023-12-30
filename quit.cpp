3e9c744f70a788b18d3e4aaa7a3150ea30c87a57
manishtyagi5 <manishtyagi.contact@gmail.com>
2023-12-30 12:10:48 +0530

    final change

--- quit.cpp
+++ quit.cpp
@@ -1,16 +1,10 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int help()
-{
-    return -1;
-}
+
 int main()
 {
-    cout<<"Hello World"<<endl;
-    cout<<"from earth"<<endl;
-    cout<<"from vishnu"<<endl;
-    int a = 10+5+10;
-    cout<<a<<endl;
-    int x = help();
+   cout<<"hello world"<<endl;
+   cout<<"hello world"<<endl;
+
 }
